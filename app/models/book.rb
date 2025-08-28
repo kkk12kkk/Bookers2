bdf39846db8fd4737fa5055679286fbbde414c44
kazunorimorishita <214490147+kkk12kkk@users.noreply.github.com>
2025-08-28 13:14:29 +0000

    8/28

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
      has_one_attached :image
      belongs_to :user  
 
-     validates :body, length: ( maximum: 200)
+     validates :body, length: { maximum: 200}
 
      validates :title, presence: true
      validates :body, presence: true
